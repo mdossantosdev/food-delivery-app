e4e81f111e1111fce9acf93b0b524a6794ad1709
Marc Dos Santos <mdossantosdev@gmail.com>
2020-10-15 14:34:30 +0200

    Add Category, FoodItem, Restaurant, FoodAvailability interfaces

--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -3,11 +3,43 @@ import { LocationObject } from 'expo-location';
 export interface User {
   firstName: string;
   lastName: string;
-  phoneNumber: string;
+  phone: string;
 }
 
 export interface UserState {
   user: User;
   location: LocationObject;
   error: string | undefined;
+}
+
+export interface Category {
+  id: string;
+  title: string;
+  icon: string;
+}
+
+export interface FoodItem {
+  _id: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  readyTime: number;
+  images: [string];
+}
+
+export interface Restaurant {
+  _id: string;
+  name: string;
+  foodType: string;
+  address: string;
+  phone: string;
+  images: string;
+  foods: [FoodItem];
+}
+
+export interface FoodAvailability {
+  categories: [Category];
+  restaurants: [Restaurant];
+  foods: [FoodItem];
 }
