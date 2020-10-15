2a9424062248ff49d6d006836c4b3e9280304f91
Marc Dos Santos <mdossantosdev@gmail.com>
2020-10-15 10:21:41 +0200

    Add types in navigation

---
+++ src/navigation/types.ts
@@ -0,0 +1,11 @@
+export type AppStackParamList = {
+  Welcome: undefined;
+  BottomTab: undefined;
+};
+
+export type BottomTabStackParamList = {
+  Home: undefined;
+  Offer: undefined;
+  Cart: undefined;
+  Account: undefined;
+};
