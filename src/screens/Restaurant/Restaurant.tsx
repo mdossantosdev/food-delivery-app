7ec2be10a4ea6a6dbdfdecbd889e82a553016243
Marc Dos Santos <mdossantosdev@gmail.com>
2020-10-22 10:49:11 +0200

    Update Restaurant screen

--- src/screens/Restaurant/Restaurant.tsx
+++ src/screens/Restaurant/Restaurant.tsx
@@ -1,12 +1,30 @@
 import React, { FC } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 import { styles } from './styles';
 
 export const Restaurant: FC = () => {
+  const route = useRoute();
+
+  const { restaurant } = route.params;
+
   return (
     <View style={styles.container}>
-      <Text>Restaurant Screen</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{restaurant.name}</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <ImageBackground
+          source={{ uri: `${restaurant.images[0]}`}}
+          style={styles.imageBackground}
+        >
+          <View style={styles.textContainer}>
+            <Text style={styles.name}>{restaurant.name}</Text>
+            <Text style={styles.address}>{restaurant.address}</Text>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
 };
--- src/screens/Restaurant/styles.ts
+++ src/screens/Restaurant/styles.ts
@@ -1,9 +1,47 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('screen').width;
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  header: {
+    flex: 1,
+    paddingLeft: 10,
+    flexDirection: 'row',
     alignItems: 'center',
-  }
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: '700',
+  },
+  contentContainer: {
+    flex: 11,
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  imageBackground: {
+    width: screenWidth,
+    height: 260,
+    justifyContent: 'flex-end',
+  },
+  textContainer: {
+    height: 100,
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    padding: 10,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  name: {
+    color: 'rgb(255, 255, 255)',
+    fontSize: 28,
+    fontWeight: '600',
+  },
+  address: {
+    color: 'rgb(255, 255, 255)',
+    fontSize: 20,
+    fontWeight: '500',
+  },
 });
