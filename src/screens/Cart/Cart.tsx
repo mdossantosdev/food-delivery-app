5d54481710de45034d5bbed8454ccdfbba9c7418
Marc Dos Santos <mdossantosdev@gmail.com>
2020-11-04 12:17:18 +0100

    Update Cart screen UI

--- src/screens/Cart/Cart.tsx
+++ src/screens/Cart/Cart.tsx
@@ -1,12 +1,48 @@
 import React, { FC } from 'react';
 import { View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { MaterialCommunityIcons as Icon } from '@expo/vector-icons';
 
 import { styles } from './styles';
+import { FoodCard } from '../../components/FoodCard';
+import { useAppSelector } from '../../hooks/reduxHooks';
 
 export const Cart: FC = () => {
+  const { cart } = useAppSelector((state) => state.user);
+
+  if (cart.length === 0) {
+    return (
+      <View style={styles.emptyCartContainer}>
+        <Text style={styles.emptyText}>Your Cart is Empty</Text>
+        <Icon name='cart-outline' style={styles.emptyIcon} />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Cart Screen</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>My Cart</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={cart}
+          keyExtractor={(item) => `${item._id}`}
+          renderItem={({ item }) =>
+            <FoodCard
+              item={item}
+              onPress={() => {}}
+            />
+          }
+        />
+      </View>
+      <View style={styles.amountContainer}>
+        <View style={styles.amountInnerContainer}>
+          <Text style={styles.totalText}>Total</Text>
+          <Text style={styles.totalText}>$ {55}</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Cart/styles.ts
+++ src/screens/Cart/styles.ts
@@ -3,7 +3,52 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  header: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: '700',
+    color: 'rgb(0, 0, 0)',
+  },
+  cardContainer: {
+    flex: 9,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  amountContainer: {
+    flex: 2,
+    padding: 10,
+  },
+  amountInnerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  totalText: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  emptyCartContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyText: {
+    fontSize: 24,
+    fontWeight: '700',
+    color: 'rgb(0, 0, 0)',
+    marginBottom: 20,
+  },
+  emptyIcon: {
+    fontSize: 42,
+    color: 'rgb(0, 0, 0)',
+  },
 });
