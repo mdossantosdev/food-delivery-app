a1c4672db828c6f708be362fde69bf3b8fc35668
Marc Dos Santos <mdossantosdev@gmail.com>
2020-11-05 16:09:12 +0100

    Update Login screen

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,12 +1,41 @@
-import React, { FC } from 'react';
-import { View, Text } from 'react-native';
+import React, { FC, useState } from 'react';
+import { View, Text, Image } from 'react-native';
 
 import { styles } from './styles';
+import { TextInput } from '../../components/TextInput';
+import { ButtonWithTitle } from '../../components/ButtonWithTitle';
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import { login } from '../../redux/user/actions';
 
 export const Login: FC = () => {
+  const dispatch = useAppDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Login Screen</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Login</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <Image style={styles.deliveryIcon} source={require('../../images/food_delivery_icon.png')} />
+        <TextInput
+          placeholder={'Email'}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          placeholder={'Password'}
+          secureText
+          onChangeText={setPassword}
+        />
+        <View style={styles.buttonContainer}>
+          <ButtonWithTitle
+            title='Login'
+            onPress={() => dispatch(login(email, password))}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -3,8 +3,29 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  header: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: '700',
+    color: 'rgb(0, 0, 0)',
+  },
+  contentContainer: {
+    flex: 11,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  deliveryIcon: {
+    width: 120,
+    height: 120,
+    marginVertical: 30,
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
