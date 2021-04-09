474fc4fbcb02a3e203ceea5d34ca0b99a38fd245
Marc Dos Santos <mdossantosdev@gmail.com>
2021-04-09 10:33:17 +0200

    Update Order screen

--- src/screens/Order/Order.tsx
+++ src/screens/Order/Order.tsx
@@ -1,16 +1,35 @@
 import React, { FC } from 'react';
 import { View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
 import { styles } from './styles';
 import { OrderNavigationProp } from './types';
+import { BackButton } from '../../components/BackButton';
+import { useAppSelector } from '../../hooks/reduxHooks';
 
 export const Order: FC = () => {
   const navigation = useNavigation<OrderNavigationProp>();
+  const { orders } = useAppSelector((state) => state.user);
 
   return (
     <View style={styles.container}>
-      <Text>Order Screen</Text>
+      <View style={styles.navigation}>
+        <View style={styles.iconContainer}>
+          <BackButton onPress={() => navigation.goBack()} />
+        </View>
+        <Text style={styles.title}>Orders</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={orders}
+          renderItem={({ item }) =>
+            <Text>{item._id}</Text>
+          }
+          keyExtractor={(item) => `${item._id}`}
+        />
+      </View>
     </View>
   )
 };
