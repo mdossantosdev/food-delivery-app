1389dc67601cd4b11d619d31c52d8668f091cb7f
Marc Dos Santos <mdossantosdev@gmail.com>
2021-04-09 10:04:13 +0200

    Add navigation types to Order screen

--- src/screens/Order/Order.tsx
+++ src/screens/Order/Order.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from 'react';
 import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { styles } from './styles';
+import { OrderNavigationProp } from './types';
 
 export const Order: FC = () => {
+  const navigation = useNavigation<OrderNavigationProp>();
+
   return (
     <View style={styles.container}>
       <Text>Order Screen</Text>
