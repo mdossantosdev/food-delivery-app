8d0cea952898eaf5e2b3a2d327d0e09d965bd05c
Marc Dos Santos <mdossantosdev@gmail.com>
2021-04-14 10:17:38 +0200

    Add Account screen

--- src/screens/Account/Account.tsx
+++ src/screens/Account/Account.tsx
@@ -1,12 +1,54 @@
 import React, { FC } from 'react';
 import { View, Text } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { MaterialCommunityIcons as Icon } from '@expo/vector-icons';
 
 import { styles } from './styles';
+import { Login } from '../Login';
+import { useAppSelector } from '../../hooks/reduxHooks';
 
 export const Account: FC = () => {
+  const { user } = useAppSelector((state) => state.user);
+
+  const options = [
+    {
+      title: 'Edit Profile',
+      action: () => {}
+    },
+    {
+      title: 'View Orders',
+      action: () => {}
+    },
+    {
+      title: 'Contact Support',
+      action: () => {}
+    }
+  ]
+
+  const optionCard = (title: string, action: Function) => {
+    return (
+      <TouchableOpacity style={styles.cardContainer} key={title} onPress={() => action()}>
+        <Text style={styles.textCard}>{title}</Text>
+        <Icon name='chevron-right' style={styles.iconCard} />
+      </TouchableOpacity>
+    )
+  }
+
+  if (!user.token) return <Login />;
+
   return (
     <View style={styles.container}>
-      <Text>Account Screen</Text>
+      <View style={styles.header}>
+        <View style={styles.avatarContainer}>
+          <Icon name='account' style={styles.avatar} />
+        </View>
+        <Text style={styles.emailText}>{user.email}</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <ScrollView>
+          { options.map(({ title, action }) => optionCard(title, action)) }
+        </ScrollView>
+      </View>
     </View>
   );
 };
--- src/screens/Account/styles.ts
+++ src/screens/Account/styles.ts
@@ -3,7 +3,48 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  header: {
+    flex: 4,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  avatarContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    marginBottom: 20,
+    backgroundColor: 'rgba(197, 197, 197, 0.8)',
+  },
+  avatar: {
+    fontSize: 56,
+    padding: 20,
+  },
+  emailText: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  contentContainer: {
+    flex: 8,
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderTopColor: 'rgb(197, 197, 197)',
+    borderTopWidth: 0.5,
+    borderBottomColor: 'rgb(197, 197, 197)',
+    borderBottomWidth: 0.5,
+  },
+  textCard: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  iconCard: {
+    fontSize: 40,
+    color: 'rgb(197, 197, 197)',
+  },
 });
