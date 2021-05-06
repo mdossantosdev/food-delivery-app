aacca0772353ee91844d2378057cd677b7e63f6e
Marc Dos Santos <mdossantosdev@gmail.com>
2021-05-06 10:49:37 +0200

    Add Offers screen

--- src/screens/Offers/Offers.tsx
+++ src/screens/Offers/Offers.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from 'react';
 import { View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
+import { useAppSelector } from '../../hooks/reduxHooks';
 
 export const Offers: FC = () => {
+  const { offers } = useAppSelector((state) => state.shop);
+
   return (
     <View style={styles.container}>
-      <Text>Offers Screen</Text>
+      <View style={styles.navigation}>
+        <Text style={styles.title}>Offers & Deals</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={offers}
+          keyExtractor={(item) => `${item._id}`}
+          renderItem={({ item }) =>
+            <Text>{item.title}</Text>
+          }
+        />
+      </View>
     </View>
   );
 };
--- src/screens/Offers/styles.ts
+++ src/screens/Offers/styles.ts
@@ -3,7 +3,21 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+  navigation: {
+    flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: '700',
+  },
+  contentContainer: {
+    flex: 11,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
