8dc316bcaf962016e7ba8fa036f96aab69134b02
Marc Dos Santos <mdossantosdev@gmail.com>
2021-10-30 14:42:13 +0200

    Add countItems() to utils

--- src/utils/cart.ts
+++ src/utils/cart.ts
@@ -4,8 +4,18 @@ export const checkExistence = (item: IFoodItem, cart: IFoodItem[]) => {
   if(Array.isArray(cart)) {
     let currentItem = cart.filter((cartItem) => cartItem._id === item._id )
     if(currentItem.length > 0) {
-        return currentItem[0]
+      return currentItem[0];
     }
   }
   return item;
+};
+
+export const countItems = (cart: IFoodItem[]): number => {
+  const totalItems = cart.reduce((accumulator: number, item: IFoodItem) => {
+    if (item.quantity) return accumulator + item.quantity;
+
+    return accumulator;
+  }, 0)
+
+  return totalItems;
 };
